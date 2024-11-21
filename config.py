cd275395c96dcf2671c44c4b1dff239bf4309d00
oscarperdomop <osdo-persa.x@hotmail.com>
2024-11-21 13:01:41 -0500

    contrasena

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
 
     # URI de conexión a la base de datos PostgreSQL
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:1082125055@localhost/saya"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:115689@localhost/saya_tv"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
